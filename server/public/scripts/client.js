793b786bf41b41b4b3477b627fe54be7ecfc0ba2
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-05-21 10:52:50 -0500

    inputs collected converted and stored

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,17 +2,34 @@ $(Ready);
 
 function Ready() {
     console.log('In onReady');
-    $('#userInputs').on('click','#plusBtn', addValues)
+    $('#userInputs').on('click','#submitBtn',collectInputsTurnToNumber )
     
 }
+// = button collects inputs and listens to see which button is clicked next which it will then call that function
+function collectInputsTurnToNumber(event) { 
+    let getValues =  $('.inputVal').map((_, el) => el.value).get()
+    let convertedValues = [];
+    
+    for (value of getValues) {
+        value = (Number(value))
+        console.log(value);
+        convertedValues.push(value)
 
+    }
+    console.log(convertedValues);
+    event.preventDefault();
+}
+
+/*
 function addValues(event) {
     console.log('In addValues');
 
-    let AddValues =  $('.inputVal').map((_, el) => el.value).get()
-    
-    console.log(AddValues);
 
-    
+    }
+    console.log(value);
+  
+
+
     event.preventDefault();
 }
+*/
