998df9b96728fa2bbbbf5a56eb2b8878c0833d12
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-05-21 16:50:34 -0500

    object sent to server btns working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,35 +1,83 @@
 $(Ready);
 
+let tacos;
+
 function Ready() {
     console.log('In onReady');
-    $('#userInputs').on('click','#submitBtn',collectInputsTurnToNumber )
+    $('#userInputs').on('click','#submitBtn', collectInputsTurnToNumber )
+    
+    $('#plusBtn').on('click', doAdditon)
+    
+    $('#minusBtn').on('click', doSubtraction)
+    
+    $('#timesBtn').on('click', doMultiplication)
+
+    $('#divideBtn').on('click', doDivison)
     
 }
+
+    
+
+ // decided to make global variable so all functions can access it
 // = button collects inputs and listens to see which button is clicked next which it will then call that function
 function collectInputsTurnToNumber(event) { 
-    let getValues =  $('.inputVal').map((_, el) => el.value).get()
-    let convertedValues = [];
+    event.preventDefault();
+     // https://stackoverflow.com/a/58050152
+    // let getValues =  $('.inputVal').map((_, el) => el.value).get()
+    //console.log(getValues);
+    let holdValues = {
+        firstInput: Number($('#firstInput').val()),
+        secondInput: Number($('#secondInput').val()),
+        operator: tacos 
+    }
+
+    console.log(holdValues);
+    // for (value of getValues) {
+    //     value = (Number(value))
+    //     console.log(value);
+    //     convertedValues.push(value)
+
+    // }
+ 
+
+    $.ajax({
+        url: '/submit-for-calc',
+        type: "POST",
+        data: holdValues
+
+    }).then(function (res){
+        console.log(res);
+    });
+
+
+    event.preventDefault();
+   
+}
+
+function doAdditon(event) {
+    event.preventDefault();
+    tacos = '+'
+    console.log('do addition',tacos);
     
-    for (value of getValues) {
-        value = (Number(value))
-        console.log(value);
-        convertedValues.push(value)
-
-    }
-    console.log(convertedValues);
-    event.preventDefault();
 }
 
-/*
-function addValues(event) {
-    console.log('In addValues');
-
-
-    }
-    console.log(value);
-  
-
-
+function doSubtraction(event) {
     event.preventDefault();
+    tacos = '-'
+    console.log('do addition',tacos);
+    
 }
-*/
+
+function doMultiplication(event) {
+    event.preventDefault();
+    tacos = '*'
+    console.log('do addition',tacos);
+    
+}
+
+function doDivison(event) {
+    event.preventDefault();
+    tacos = '/'
+    console.log('do addition',tacos);
+    
+}
--- server/server.js
+++ server/server.js
@@ -6,6 +6,57 @@ const bodyParser = require("body-parser");
 
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+let totalValue = [];
+
+app.post('/submit-for-calc', (req,res) => {
+
+    let holdValues = req.body
+    
+    console.log('checking object',req.body); // = holdvalues 
+    console.log('In server.js');
+    
+    totalValue.push(req.body)
+    console.log('The total value is',totalValue);
+
+    doMath(holdValues)
+    res.sendStatus(201)
+})
+
+
+function doMath(holdValues) {
+    if (holdValues.operator === '+') {
+        console.log('Addition');
+    } else if (holdValues.operator === '-') {
+        console.log('Subtraction');
+    } else if (holdValues.operator === '*') {
+        console.log('Multiply');
+    } else if (holdValues.operator === '/')
+    console.log('divide');
+}
+  
+
+   
+
+    /*
+    $('#minusBtn').on('click',subtractValues)
+       
+
+    $('#timesBtn').on('click',multiplyValues)
+        
+
+    $('#divideBtn').on('click',divideValues)
+        */
+
+
+
+
+ // decided to make global variable so all functions can access it
+// = button collects inputs and listens to see which button is clicked next which it will then call that function
+
+// console log what operation is clicked
 
 app.listen(5000, () => {
     console.log('Siri is Lisitening');
